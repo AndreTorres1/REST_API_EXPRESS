b6449f2134683fa425cfb829ba1d95551ac8a9a4
AndreTorres1 <mcpanda700@gmail.com>
2022-10-17 15:15:54 +0100

    Delete a funcionar para os movies e as categorias.

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ let movies = []
 
 let category = []
 
+
 //sempre que alguem tenta aceder ao localhost:port irá parar a este path
 app.get('/', (req, res) => {
     res.json({
@@ -27,6 +28,17 @@ app.get('/api/category', (req, res) => {
     res.status(200).json(category);
 });
 
+//Vai buscar um filme com um ID especifico
+app.get('/api/movies/:id',(req, res) => {
+    const {id} = req.params;
+    const getMovieByID = movies.find(movie => movie.id === id);
+
+    if (getMovieByID){
+        res.status(200).json(getMovieByID)
+    } else {
+        res.status(404).json({message: "O filme nao existe"})
+    }
+});
 
 //------------------------------------POST AREA-----------------------------------------\\
 //guarda a informacao do POST no req(request) *1   ******MOVIES*******
@@ -37,6 +49,7 @@ app.post('/api/movies', (req, res) => {
     movies.push(movieInfo);
     res.status(201).json(movieInfo)
 });
+
 
 //                                               ******CATEGORIAS*******
 app.post('/api/category', (req, res) => {
@@ -49,14 +62,11 @@ app.post('/api/category', (req, res) => {
 
 //------------------------------------DELETE AREA-----------------------------------------\\
 
-app.delete("api/movies/:id", (req, res) => {
-    //transforma o id de um body em params retirando assim toda a informacao associada a esse id
+app.delete('/api/movies/:id', (req, res) => {
     const { id } = req.params;
     //verifica se existisse, se nao exisitir nao apaga(pk nao tem nada para apagar -.-)
     const deleted = movies.find(movie => movie.id === id);
-console.log(deleted)
     if (deleted) {
-        console.log();
         //criamos um array novo e passamos o array gerado pelo id fornecido, ou seja ele pega num array novo pega no que é para ser apagado e transforma-o no array novo criado
         movies = movies.filter(movie => movie.id !== id)
         res.status(200).json(deleted);
@@ -65,6 +75,23 @@ console.log(deleted)
         res
             .status(404)
             .json({message: "Este filme nao existe!"})
+    }
+});
+
+//                                                  ****CATEGORIAS******
+app.delete('/api/category/:id', (req, res) => {
+    const { id } = req.params;
+    //verifica se existisse, se nao exisitir nao apaga(pk nao tem nada para apagar -.-)
+    const deleted = category.find(categ => categ.id === id);
+    if (deleted) {
+        //criamos um array novo e passamos o array gerado pelo id fornecido, ou seja ele pega num array novo pega no que é para ser apagado e transforma-o no array novo criado
+        category = category.filter(categ => categ.id !== id)
+        res.status(200).json(deleted);
+    } else {
+        console.log(deleted);
+        res
+            .status(404)
+            .json({message: "Esta categoria nao existe!"})
     }
 });
 
